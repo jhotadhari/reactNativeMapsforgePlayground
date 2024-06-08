62ec3373de103f198c78a85623e54c8154c3ad71
jhotadhari <tellme@waterproof-webdesign.info>
2024-06-08 17:46:47 +0200

    Ask for MANAGE_APP_ALL_FILES_ACCESS_PERMISSION on app start

--- android/app/src/main/java/com/jhotadhari/reactNativeMapsforgePlayground/MainActivity.kt
+++ android/app/src/main/java/com/jhotadhari/reactNativeMapsforgePlayground/MainActivity.kt
@@ -4,8 +4,28 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
 
 class MainActivity : ReactActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R &&
+            !Environment.isExternalStorageManager()) {
+            val intent =
+                Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+            val uri = Uri.fromParts("package", packageName, null)
+            intent.data = uri
+            startActivity(intent)
+        }
+    }
+
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
